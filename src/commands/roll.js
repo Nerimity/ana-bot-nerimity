7fe53834550f74991ec4340ec47c62db1576636a
Supertiger <saawalkhn@googlemail.com>
2025-06-08 20:56:01 +0100

    Add roll dice command

---
+++ src/commands/roll.js
@@ -0,0 +1,19 @@
+import Roll from "roll";
+
+export const command = "roll";
+export const description = "Roll a dice.";
+export const args = "<dice>";
+
+
+const roll = new Roll();
+
+/**
+ * @param {import("@nerimity/nerimity.js/build/Client.js").Message} message
+ */
+export const run = async (bot, args, message) => {
+  const argsWithoutFirst = args.slice(1);
+
+  const rollOutput = roll.roll(argsWithoutFirst.join(" "));
+
+  return message.reply(rollOutput.result)
+};
