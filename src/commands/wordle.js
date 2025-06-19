94914e3d9f2d7138c3c480c9bd9f756d5804b3c9
Supertiger <saawalkhn@googlemail.com>
2025-06-19 14:08:52 +0100

    fix: add instruction message for starting the Wordle game

--- src/commands/wordle.js
+++ src/commands/wordle.js
@@ -133,6 +133,7 @@ export const run = async (bot, args, message) => {
   if (subCommand === "start") {
     return startCommand(bot, args, message);
   }
+  channel.send(`To start a game, run /wordle ${args}`)
 };
 
 /**
